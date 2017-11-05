008d62e7e11499ca408ced5422eb83f5a4827047
foxx1337 <foxx1337@yahoo.com>
2017-11-05 19:02:32 +0100

    Initial commit

--- PsRun/App.xaml.cs
+++ PsRun/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Management.Automation;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Collections.ObjectModel;
 
 namespace PsRun
 {
@@ -13,5 +16,45 @@ namespace PsRun
     /// </summary>
     public partial class App : Application
     {
+        public const string APP_NAME = "PsRun";
+
+        public void EntryPoint(object sender, StartupEventArgs eventArgs)
+        {
+            if (eventArgs.Args.Length != 1)
+            {
+                MessageBox.Show(
+                    string.Format("Usage:\n{0} script.ps1\n\nRuns the script in a headless PowerShell", APP_NAME),
+                    APP_NAME,
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+            else
+            {
+                try
+                {
+                    var result = RunScript(eventArgs.Args[0]);
+                    if (result.Count > 0)
+                    {
+                        // replace this with a selectable window
+                        MessageBox.Show("Script returned: " + result.ToString(), APP_NAME,
+                            MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Got an error: " + e.ToString(), APP_NAME,
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            Shutdown();
+        }
+
+        public Collection<PSObject> RunScript(string path)
+        {
+            string commands = File.ReadAllText(path);
+            PowerShell powerShell = PowerShell.Create();
+            powerShell.AddScript(commands);
+            return powerShell.Invoke();
+        }
     }
 }
